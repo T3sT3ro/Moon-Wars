3120cd76b52def440e6313f53a81b13d2dc54783
Rufusmen <rafusmen@gmail.com>
2018-05-27 23:08:22 +0200

    Hot fix on Map module. Added dummy textures

--- game/GameMap.lua
+++ game/GameMap.lua
@@ -4,7 +4,7 @@ local GameMap = {}
 
 local map = {}
 
-local mapType = {"grass","rock","water"}
+local mapType = {"grass","stone","water"}
 --[[
     map types:
     1 - grass
@@ -16,14 +16,21 @@ function GameMap.init()
         map[i] = {}
         for j=1,20 do
             map[i][j] = {}
-            map[i][j].type = 1
+            map[i][j].actors = {}
+            ---[[
+                map[i][j].type = 1
+                if i + 1 == j and j % 2 == 0 then map[i][j].type = 2 end
+                if ((i + j) % 5 == 0 and i % 4 == 0) or ((i + j) % 5 == 1 and i % 4 == 0) or ((i + j) % 5 == 0 and i % 4 == 3) or ((i + j) % 5 == 4 and i % 4 == 3)then map[i][j].type = 3 end
+            --]]
         end
     end
     ---[[
         local nexus ={}
-        nexus.asset = "dagger2"
-        map[10][5].unit = nexus
-        map[10][15].unit = nexus
+        nexus.type = "nexus_red"
+        nexus.id = 1
+        nexus.x = 5
+        nexus.y = 5
+        GameMap.addActor(nexus)
     --]]
 end
 
@@ -38,9 +45,10 @@ end
 function GameMap.draw()
     for i=1,20 do
         for j=1,20 do
-            --love.graphics.draw(RM.get(mapType[map[i][j].type]), i*20, j*20)
-            love.graphics.draw(RM.get("dagger"), i*60, j*60)
-            if map[i][j].unit ~= nil then love.graphics.draw(RM.get(map[i][j].unit.asset), i*60, j*60) end
+            love.graphics.draw(RM.get(mapType[map[i][j].type]), j*64, i*64)
+            for _,v in pairs(map[i][j].actors) do
+                love.graphics.draw(RM.get(v.type), j*64, i*64)
+            end
         end
     end
 end
@@ -50,17 +58,19 @@ function GameMap.distance(x1, y1, x2, y2)
 end
 
 function GameMap.isMoveable(x, y)
-    if map[x][y].unit == nil and map[x][y].type == 1 then return true
-    else return false
+    if ma[x][y].type ~= 1 then return false end
+    for i,_ in pairs(map[x][y].actors) do
+        if i.type ~= "item" then return false end
     end
+    return true
 end
 
 function GameMap.removeActor(actor)
-    map[actor.x][actor.y].unit = nil
+    map[actor.x][actor.y].actors[actor.id] = nil
 end
 
 function GameMap.addActor(actor)
-    map[actor.x][actor.y].unit = actor
+    map[actor.x][actor.y].actors[actor.id] = actor
 end
 
 return GameMap
