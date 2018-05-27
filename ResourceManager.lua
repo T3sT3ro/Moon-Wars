3d42dd9dbc4b1722e66b9b55a56fb8465a39cf0c
Bartosz Rudzki (HomeSmall) <bartoszrudzkilo14@gmail.com>
2018-05-27 14:55:22 +0200

    ResourceManager implemented.

--- ResourceManager.lua
+++ ResourceManager.lua
@@ -3,8 +3,11 @@ local _assets = {}
 
 function ResourceManager.init(resourcesFolderPath)
     local assets = love.filesystem.getDirectoryItems(resourcesFolderPath)
-    for k, file in ipairs(assets) do
-        print(k .. ". " .. file)
+    for _, file in ipairs(assets) do
+        local dotPos = file:find("%.")
+        local assetName = file:sub(1, dotPos-1)
+        local extension = file:sub(dotPos+1)
+        ResourceManager.load(assetName, extension, resourcesFolderPath)
     end
 end
 
@@ -15,16 +18,19 @@ end
 function ResourceManager.load(assetName, extension, path)
     path = path or ""
     local fullPath = path .. "/" .. assetName .. "." .. extension
-    _assets[assetName] = love.graphics.newImage(fullPath)
+    local asset = love.graphics.newImage(fullPath)
+
+    print("Asset: " .. assetName .. " loaded from path: " .. fullPath)
+    _assets[assetName] = asset
 end
 
-function ResourceManager.unload(assetName)
+function ResourceManager.unloadAsset(assetName)
     _assets[assetName] = nil
 end
 
 function ResourceManager.get(assetName)
     local asset = _assets[assetName]
-    if asset == nil then error("Asset with name " .. assetName .. "was not loaded!") end
+    if asset == nil then error("Asset with name " .. assetName .. " was not loaded!") end
     return asset
 end
 
