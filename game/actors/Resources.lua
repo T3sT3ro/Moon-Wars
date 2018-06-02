6f2c488e37ace6383af8f8d2376db05415ea596e
Bartosz Rudzki (Home) <bartosz.rudzki@baadgames.com>
2018-06-02 11:31:21 +0200

    Configs for items and resources.

---
+++ game/actors/Resources.lua
@@ -0,0 +1,22 @@
+local Resources = 
+{
+    {
+        name = "tree",
+        matName = "wood",
+        dropRate = 3
+    },
+
+    {
+        name = "rock",
+        matName = "stone",
+        dropRate = 2
+    },
+
+    {
+        name = "crystalMine",
+        matName = "crystal",
+        dropRate = 1
+    }
+}
+
+return Resources
