924c5e3fbc6b8ffa37809f0e6d61287cb4935748
Rufusmen <rafusmen@gmail.com>
2018-06-13 14:07:41 +0200

    Adds AI API'

--- game/GameMap.lua
+++ game/GameMap.lua
@@ -36,15 +36,21 @@ local function check()
         vis[x][y] = true
         if x == 10 then pass = true end
         if #map[x][y].actors > 1 then err = false end
-        if map[x][y].actors[1] ~= nil and map[x][y].actors[1].type == "Resource" then 
-            if map[x][y].actors[1].resType == "tree" then
-                tree = tree + 1
-            elseif map[x][y].actors[1].resType == "rock" then
-                rock = rock + 1
-            elseif map[x][y].actors[1].resType == "mine" then
-                mine = mine + 1
-            end
+        if res[1].mine.x == x and  res[1].mine.y == y then 
+            mine = mine + 1 
             return
+        end
+        for i=1,2 do 
+            if res[1].rock[i].x == x and  res[1].rock[i].y == y then 
+                rock = rock + 1 
+                return
+            end
+        end
+        for i=1,3 do 
+            if res[1].tree[i].x == x and  res[1].tree[i].y == y then 
+                tree = tree + 1 
+                return
+            end
         end
         if x < 10 and map[x+1][y].type == 1 then dfs(x+1,y) end
         if x > 1 and map[x-1][y].type == 1 then dfs(x-1,y) end
@@ -52,12 +58,15 @@ local function check()
         if y > 1 and map[x][y-1].type == 1 then dfs(x,y-1) end
     end
     dfs(nex.x,nex.y)
-    if --[[tree == 3 and --[[rock == 2 and mine == 1 and]] pass then return true end
+    if tree == 3 and rock == 2 and mine == 1 and pass then return true end
     return false
 end
 
-function GameMap.init()
-    for i=1,20 do
+function GameMap.init(MapAIPath)
+    if MapAIPath ~= nil then 
+        AI = require(MapAIPath)
+    end
+    --[[for i=1,20 do
         map[i] = {}
         for j=1,20 do
             map[i][j] = {}
@@ -66,20 +75,33 @@ function GameMap.init()
                 map[i][j].type = 1
                 if i + 1 == j and j % 2 == 0 and i <= 10 then map[i][j].type = 2 end
                 if ((i + j) % 5 == 0 and i % 4 == 0) or ((i + j) % 5 == 1 and i % 4 == 0) or ((i + j) % 5 == 0 and i % 4 == 3) or ((i + j) % 5 == 4 and i % 4 == 3) and i <= 10 then map[i][j].type = 3 end
-            --]]
+        end
+    end--]]
+    map = AI.getMap()
+    for i=1,10 do
+        for j=1,20 do
+            map[i][j].actors = {}
         end
     end
-    for i=1,20 do
+    for i=11,20 do
+        map[i] = {}
         for j=1,20 do
-            if i > 10 then map[i][j].type = map[21-i][21-j].type end
+            map[i][j] = {}
+            map[i][j].actors = {}
+            map[i][j].type = map[21-i][21-j].type
         end
     end
 
-    ---[[
+    --[[
         nex.x,nex.y = 6,5
         res[1].tree = {{x = 1, y = 1},{x = 2 , y = 15},{x = 9, y = 11}}
         res[1].rock = {{x = 6, y = 8},{x = 3 ,y = 14}}
         res[1].mine = {x = 2 , y = 10}
+        --]]
+        nex = AI.getNexus()
+        res[1].tree = AI.getTrees()
+        res[1].rock = AI.getRocks()
+        res[1].mine = AI.getMine()
         res[2].tree = {}
         res[2].rock = {}
         res[2].mine = {}
@@ -93,10 +115,10 @@ function GameMap.init()
         end
         res[2].mine.x,res[2].mine.y = 21 -res[1].mine.x,21 - res[1].mine.y
         --]]
-    --[[
+
     if check() == false then 
         error("Wrong map generated")
-    end]]
+    end
 end
 
 local function addActTable(tab)
--- game/MapAI.lua
+++ game/MapAI.lua
@@ -1,14 +1,48 @@
 local MapAI = {}
 
-function MapAI.check(map,nexus)
-    local tree,rock,crystal,pass = false,false,false,false
-    local function bfs()
+local map ={}
+
+local nex = {}
+
+local res = {}
+
+for i=1,10 do
+    map[i] = {}
+    for j=1,20 do
+        map[i][j] = {}
+        map[i][j].actors = {}
+        ---[[
+            map[i][j].type = 1
+            if i + 1 == j and j % 2 == 0 and i <= 10 then map[i][j].type = 2 end
+            if ((i + j) % 5 == 0 and i % 4 == 0) or ((i + j) % 5 == 1 and i % 4 == 0) or ((i + j) % 5 == 0 and i % 4 == 3) or ((i + j) % 5 == 4 and i % 4 == 3) and i <= 10 then map[i][j].type = 3 end
+        --]]
     end
 end
 
+nex.x,nex.y = 6,5
+res.tree = {{x = 1, y = 1},{x = 2 , y = 15},{x = 9, y = 11}}
+res.rock = {{x = 6, y = 8},{x = 3 ,y = 14}}
+res.mine = {x = 2 , y = 10}
 
 
+function MapAI.getMap()
+    return map    
+end
 
+function MapAI.getNexus()
+    return nex   
+end
 
+function MapAI.getTrees()
+    return res.tree  
+end
+
+function MapAI.getRocks()
+    return res.rock  
+end
+
+function MapAI.getMine()
+    return res.mine    
+end
 
 return MapAI
